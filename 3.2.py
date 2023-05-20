272dc4e0157ffb9f4debc6a12d345148512ffede
melisLu <mashkaromashka765@gmail.com>
2023-05-20 11:55:27 +0300

    first commit

--- 3.2.py
+++ 3.2.py
@@ -7,7 +7,7 @@ class Student:
         self.progress=0
         self.alive=True
 
-    def to_dtudy(self):
+    def to_study(self):
         print("Time to study")
         self.progress +=0.12
         self.gradness-=3
@@ -33,6 +33,44 @@ class Student:
         elif self.progress > 5:
             print('Passed externally...')
             self.alive=False
+        if self.progress < -0.3:
+            print('You need to study better')
+        elif self.progress > 1:
+            print('you need sleep')
+
+    def end_of_day(self):
+        print(f'Gladness = {self.gradness}')
+        print(f'Progress = {round(self.progress, 2)}')
+
+    def live(self,day):
+        day = "Day" + str(day) + "of" + self.name + "life"
+        print(f'{day:=^50}')
+        print('1 - to study')
+        print('2 - to sleep')
+        print('3 - to chill')
+        live_cube = (input('Enter your choice'))
+        if live_cube == '1':
+            print('You entered 1 variant(study)')
+            self.to_study()
+        elif  live_cube == '2':
+            print('You entered 2 variant(sleep)')
+            self.to_sleep()
+        elif  live_cube == '3':
+            print('You entered 3 variant(chill)')
+            self.to_chill()
+        else:
+            print('Not correct choice.You can enter only 1 or 2 or 3')
+        self.end_of_day()
+        self.is_alive()
+
+nick=Student(name='Nick')
+for day in range (365):
+    if nick.alive==False:
+        print('Student lose')
+        break
+    nick.live(day)
+
+
 
 
 
